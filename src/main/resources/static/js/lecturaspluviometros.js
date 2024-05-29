fd3fc26ffe0da26e8a135b85c3794bd3dbc789b3
Joseluisct <joseluisct1@gmail.com>
2024-05-29 11:11:08 +0200

    revertir cambio

---
+++ src/main/resources/static/js/lecturaspluviometros.js
@@ -0,0 +1,120 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+    alert('Hola');
+    cargarPluviometros();
+    $('#pluviometros').DataTable();
+    actualizarEmailUsuario();
+    obtenerCantidadPluviometros();
+});
+
+function actualizarEmailUsuario() {
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email;
+}
+
+async function cargarPluviometros() {
+    const request = await fetch('api/pluviometros', {
+        method: 'GET',
+        headers: getHeaders(),
+    });
+    const pluviometros = await request.json();
+
+    let listadoPluviometrosHtml = '';
+    for (let pluviometro of pluviometros) {
+        let botonEliminar = '<a href="#" onclick="eliminarPluviometro(' + pluviometro.id + ')" class="btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i> </a>'
+        let pluviometroHtml = '<tr><td>' + pluviometro.id + '</td> <td>' + pluviometro.nombre + '</td> <td>'
+            + pluviometro.latitud + '</td> <td>'
+            + pluviometro.longitud + '</td> <td> ' + botonEliminar + ' </td></tr>';
+        listadoPluviometrosHtml += pluviometroHtml;
+    }
+    document.querySelector('#pluviometros tbody').outerHTML = listadoPluviometrosHtml;
+}
+
+// Funcion para devolver el headers
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.token
+    }
+
+}
+// Función para obtener el número de registros de la tabla
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+    cargarPluviometros();
+    $('#pluviometros').DataTable();
+    actualizarEmailUsuario();
+});
+
+function actualizarEmailUsuario() {
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email;
+}
+
+async function cargarPluviometros() {
+    const request = await fetch('api/pluviometros', {
+        method: 'GET',
+        headers: getHeaders(),
+    });
+    const pluviometros = await request.json();
+
+    let listadoPluviometrosHtml = '';
+    for (let pluviometro of pluviometros) {
+        let botonEliminar = '<a href="#" onclick="eliminarPluviometro(' + pluviometro.id + ')" class="btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i> </a>'
+        let pluviometroHtml = '<tr><td>' + pluviometro.id + '</td> <td>' + pluviometro.nombre + '</td> <td>'
+            + pluviometro.latitud + '</td> <td>'
+            + pluviometro.longitud + '</td> <td> ' + botonEliminar + ' </td></tr>';
+        listadoPluviometrosHtml += pluviometroHtml;
+    }
+    document.querySelector('#pluviometros tbody').outerHTML = listadoPluviometrosHtml;
+}
+
+// Funcion para devolver el headers
+function getHeaders() {
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.token
+    }
+
+}
+// Función para obtener el número de registros de la tabla
+
+async function obtenerCantidadPluviometros() {
+    const request = await fetch('api/pluviometros/count', {
+        method: 'GET',
+        headers: getHeaders(),
+    });
+    const cantidad = await request.json();
+    mostrarCantidadPluviometros(cantidad); // Llamar a la función mostrarCantidadPluviometros con la cantidad obtenida
+}
+
+function mostrarCantidadPluviometros(cantidad) {
+    // Mostrar la cantidad en el HTML
+    const cantidadElement = document.getElementById('cantidad-pluviometros');
+    if (cantidadElement) {
+        cantidadElement.textContent = cantidad.toString();
+    }
+}
+
+async function eliminarPluviometro(id) {
+    if (!confirm('¿Está seguro de eliminar el pluviómetro?')) {
+        return;
+    }
+    const request = await fetch('api/pluviometros/' + id, {
+        method: 'DELETE',
+        headers: getHeaders()
+    });
+    location.reload();
+}
+
+
+async function eliminarPluviometro(id) {
+    if (!confirm('¿Está seguro de eliminar el pluviómetro?')) {
+        return;
+    }
+    const request = await fetch('api/pluviometros/' + id, {
+        method: 'DELETE',
+        headers: getHeaders()
+    });
+    location.reload();
+}
---
+++ src/main/resources/static/js/registrarPluviometro.js
@@ -0,0 +1,42 @@
+// Inicializa el plugin de jQuery dataTables cuando el documento está listo
+$(document).ready(function() {
+    // on Ready
+});
+
+// Función asíncrona para registrar un usuario
+async function registrarUsuario() {
+    // Recoge los valores de los campos de entrada del formulario y los almacena en un objeto
+    let datos = {
+        nombre: document.getElementById('txtNombre').value,
+        apellido  : document.getElementById('txtApellidos').value,
+        email: document.getElementById('txtEmail').value,
+        password  : document.getElementById('txtPassword').value,
+    };
+
+    // Recoge el valor del campo de entrada 'Repetir Contraseña'
+    let repetirPassword = document.getElementById('txtRepetirPassword').value;
+
+    // Comprueba si la contraseña y la repetición de la contraseña son iguales
+    if  (datos.password != repetirPassword) {
+        // Si las contraseñas no coinciden, muestra una alerta al usuario y detiene la ejecución de la función
+        alert('Las contraseñas no coinciden');
+        return;
+    }
+
+    // Si las contraseñas coinciden, realiza una solicitud POST a la API de usuarios con los datos del formulario en formato JSON
+    const request = await fetch('api/usuarios', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    });
+
+    // Una vez que la solicitud se ha completado, muestra una alerta al usuario indicando que el registro se ha realizado correctamente
+    alert("Usuario registrado correctamente");
+
+    // Redirige al usuario a la página de inicio de sesión
+    window.location.href = 'login.html';
+}
+
